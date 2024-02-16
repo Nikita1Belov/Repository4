59e6af9cfc48ea3896efa4400496b2d531b6401b
Nikita <118474494+Nikita1Belov@users.noreply.github.com>
2024-02-16 21:53:37 +0200

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,34 @@
+package Calculator;
+class Calculator {
+    private int result;
+
+    public Calculator() {
+        reset();
+    }
+
+    public void reset() {
+        this.result = 0;
+    }
+
+    public int getResult() {
+        return this.result;
+    }
+
+    public void add(int value) {
+        this.result += value;
+    }
+
+    public void subtract(int value) {
+        this.result -= value;
+    }
+
+    public void multiply(int value) {
+        this.result *= value;
+    }
+
+    public void exponent(int value) {
+        for(int i=1; i<value; i++) {
+            multiply(this.result);
+        }
+    }
+}
